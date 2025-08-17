ea907293ab9394fa54c92907d4bc8ca2eb4c1a86
Niko-Borbe <226933079+Niko-Borbe@users.noreply.github.com>
2025-08-17 11:39:12 +0800

    deleted:    strings.py

--- strings.py
+++
@@ -1,4 +0,0 @@
-# First file(or notes) for the series!
-# Here we will focus in the basics of strings and characters in python, also including formatting, concatenation, and variables in string.
-# Note! that variables will be talked in-depth next time.
-
