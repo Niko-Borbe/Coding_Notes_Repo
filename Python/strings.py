4448efa852bf0118081f00ceaa153bbea338fba6
Niko-Borbe <226933079+Niko-Borbe@users.noreply.github.com>
2025-08-17 11:34:45 +0800

    py folder added

---
+++ Python/strings.py
@@ -0,0 +1,4 @@
+# First file(or notes) for the series!
+# Here we will focus in the basics of strings and characters in python, also including formatting, concatenation, and variables in string.
+# Note! that variables will be talked in-depth next time.
+
